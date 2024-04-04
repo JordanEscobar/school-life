fbc614a69a029af4b8923151ce40fa207ef69569
Jordan Escobar <jordanescosoto@gmail.com>
2024-04-03 23:08:28 -0300

    First commit
    Commit

---
+++ src/main/java/com/schoollife/classbook/SchoolLifeApplication.java
@@ -0,0 +1,13 @@
+package com.schoollife.classbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoollife/classbook/SchoolLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoollife.classbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
